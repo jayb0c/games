196c7611c2323ce87de27e86b3ca69343c7bf344
Jake Stone <Stonedev12@gmail.com>
2023-02-23 21:48:45 -0800

    I have created the first shape and I have made it stop at the bottom of the canvas.

--- canvas.js
+++ canvas.js
@@ -4,22 +4,34 @@ function draw(move){
   let ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.moveTo(0, 0 + move);
-    ctx.lineTo(75, 0 + move);
-    ctx.lineTo(75, 25 + move);
-    ctx.lineTo(50, 25 + move);
-    ctx.lineTo(50, 50 + move);
-    ctx.lineTo(25, 50 + move);
-    ctx.lineTo(25, 25 + move);
-    ctx.lineTo(0, 25 + move);
+    if(move < 700){
+    ctx.moveTo(250, 0 + move);
+    ctx.lineTo(325, 0 + move);
+    ctx.lineTo(325, 25 + move);
+    ctx.lineTo(300, 25 + move);
+    ctx.lineTo(300, 50 + move);
+    ctx.lineTo(275, 50 + move);
+    ctx.lineTo(275, 25 + move);
+    ctx.lineTo(250, 25 + move);
     ctx.fillStyle = "#FF8001";
     ctx.fill();
-    console.log(move);
+    } else {
+      ctx.moveTo(250, 700);
+      ctx.lineTo(325, 700);
+      ctx.lineTo(325, 725);
+      ctx.lineTo(300, 725);
+      ctx.lineTo(300, 750);
+      ctx.lineTo(275, 750);
+      ctx.lineTo(275, 725);
+      ctx.lineTo(250, 725);
+      ctx.fillStyle = "#FF8001";
+      ctx.fill();
+    }
 }
 
 let count = 0;
 setInterval(() => {
-  count+= 2;
+  count+= 4;
   draw(count);
   console.log(count);
 }, 100);
